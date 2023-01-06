e18449362332bec46285e8ee03d9fad7c1647652
Rifqi Fauzi Ramdhani <46875859+muhammadrifqifr@users.noreply.github.com>
2023-01-06 17:17:37 +0700

    updated

--- daos/DivisionDAO.java
+++ daos/DivisionDAO.java
@@ -35,72 +35,72 @@ public class DivisionDAO {
         }
         return divisions;
     }
-}
+
 
 // // Get by ID
-//     public Division getById(int id){
-//         String query = "SELECT * FROM tb_m_division WHERE Id = ?";
-//         Division division = new Division();
-//         try {
-//             PreparedStatement prepareStatement  = con.prepareStatement(query);
-//             prepareStatement.setInt(1,id);
-//             ResultSet resultSet = prepareStatement.executeQuery();
-//             while (resultSet.next()){
-//                 region.setId(resultSet.getInt(1));
-//                 region.setName(resultSet.getString(2));
-//             }
-//         } catch (Exception e) {
-//             e.printStackTrace();
-//         }
-//         return division;
-//     }
+    public Division getById(int id){
+        String query = "SELECT * FROM tb_m_division WHERE Id = ?";
+        Division division = new Division();
+        try {
+            PreparedStatement prepareStatement  = con.prepareStatement(query);
+            prepareStatement.setInt(1,id);
+            ResultSet resultSet = prepareStatement.executeQuery();
+            while (resultSet.next()){
+                division.setId(resultSet.getInt(1));
+                division.setName(resultSet.getString(2));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return division;
+    }
 
     //Insert
-    // public boolean insert(Division division){
-    //     try {
-    //         PreparedStatement preparedStatement = con.prepareStatement("INSERT INTO tb_m_division(name) VALUES(?)");
-    //         preparedStatement.setInt(1, division.getId());
-    //         preparedStatement.setString(2, division.getName());
-    //         //preparedStatement.setInt(3, division.getRegionId());
-    //         int temp = preparedStatement.executeUpdate();
-    //         //System.out.println("Insert Data Division Success");
-    //         return temp > 0;
-    //     } catch (SQLException e){
-    //         e.printStackTrace();
-    //     }
-    //     return false;
-    // }
+    public boolean insert(Division division){
+        try {
+             PreparedStatement preparedStatement = con.prepareStatement("INSERT INTO tb_m_division(name) VALUES(?)");
+             //preparedStatement.setInt(1, division.getId());
+             preparedStatement.setString(1, division.getName());
+             //preparedStatement.setInt(3, division.getRegionId());
+             int temp = preparedStatement.executeUpdate();
+             //System.out.println("Insert Data Division Success");
+             return temp > 0;
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return false;
+    }
 
     //Update
-    // public boolean update(Division division){
-    //     try {
-    //         String query = "UPDATE tb_m_division SET id = ?, name = ? WHERE Id = ?";
-    //         PreparedStatement preparedStatement = con.prepareStatement(query);
-    //         preparedStatement.setInt(1, division.getId());
-    //         preparedStatement.setString(2, division.getnName());
-    //         //preparedStatement.setInt(3, division.getRegionId());
-    //         preparedStatement.setInt(3, division.getId());
-    //         int temp = preparedStatement.executeUpdate();
-    //         return temp > 0;
-    //     } catch (SQLException e){
-    //         e.getMessage();
-    //     }
-    //     return false;
-    // }
+    public boolean update(Division division){
+        try {
+            String query = "UPDATE tb_m_division SET id = ?, name = ? WHERE Id = ?";
+            PreparedStatement preparedStatement = con.prepareStatement(query);
+            preparedStatement.setInt(1, division.getId());
+            preparedStatement.setString(2, division.getnName());
+            preparedStatement.setInt(3, division.getRegionId());
+            preparedStatement.setInt(3, division.getId());
+            int temp = preparedStatement.executeUpdate();
+            return temp > 0;
+        } catch (SQLException e){
+             e.getMessage();
+        }
+        return false;
+    }
 
     //Delete
-    // public boolean delete(Integer Id){
-    //     try {
-    //         String query = "DELETE FROM tb_m_division WHERE id = ?";
-    //         PreparedStatement preparedStatement = con.prepareStatement(query);
-    //         preparedStatement.setInt(1, Id);
-    //         int temp = preparedStatement.executeUpdate();
-    //         return temp > 0;
-    //     } catch (SQLException e){
-    //         e.printStackTrace();
-    //     }
-    //     return false;
-    // }
+    public boolean delete(Integer Id){
+         try {
+            String query = "DELETE FROM tb_m_division WHERE id = ?";
+            PreparedStatement preparedStatement = con.prepareStatement(query);
+            preparedStatement.setInt(1, Id);
+            int temp = preparedStatement.executeUpdate();
+            return temp > 0;
+        } catch (SQLException e){
+             e.printStackTrace();
+         }
+         return false;
+    }
 
 	// public Object getConnection() {
 	// 	return null;
