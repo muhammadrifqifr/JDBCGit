830a70aa71be1b07aaac6d25d3fbc658547eaea4
Rifqi Fauzi Ramdhani <46875859+muhammadrifqifr@users.noreply.github.com>
2023-01-06 11:06:11 +0700

    updated

--- controllers/DivisionController.java
+++ controllers/DivisionController.java
@@ -6,16 +6,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.daos.DivisionDAO;
+import com.example.demo.models.Division;
 import com.example.demo.tools.DBConnection;
 
 @Controller
 @RequestMapping("division")
 public class DivisionController {
-    private DivisionDAO division = new DivisionDAO(DBConnection.getConnection());
+    private DivisionDAO divisionDAO = new DivisionDAO(DBConnection.getConnection());
 
+    // GET ALL
     @GetMapping
     public String index(Model model){
-        model.addAttribute( "divisions", division.getConnection());
-        return "";
+        Division division = new Division();
+        division.setId(3);
+        division.setName("Sport");
+        division.setRegion();
+        model.addAttribute( "divisions", divisionDAO.getAll());
+        return "division/index";
     }
 }
+    // GET 
+    // @GetMapping (value = {"form/{id}"})
+    // public String 
+
