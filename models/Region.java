02dcead000dde1247b51d2810566d2522584c11e
Rifqi Fauzi Ramdhani <46875859+muhammadrifqifr@users.noreply.github.com>
2023-01-06 09:06:26 +0700

    updated

--- models/Region.java
+++ models/Region.java
@@ -1,19 +1,34 @@
-package models;
+package com.example.demo.models;
 
+import javax.persistence.*;
+//import javax.persistence.GenerationType;
+
+@Entity
+@Table(name = "tb_m_region")
 public class Region {
-    private int regionId;
-    private String regionName;
 
-    public int getRegionId(){
-        return this.regionId;
+   
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    //@OneToMany(mappedBy = "region", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
+    //@JoinColumn(name = "reg_div", JoinColumn = @JoinColumn(name = "id"), inverseJoinColumns = @JoinColumn(name = "id"))
+
+    private Integer id;
+    private String name;
+    @Column(name = "name")
+
+    public Integer getId(){
+        return id;
     }
-    public void setRegionId(int regionId){
-        this.regionId = regionId;
+    public void setId(Integer id){
+        this.id = id;
     }
-    public String getRegionName(){
-        return this.regionName;
+    public String getName(){
+        return name;
     }
-    public void setRegionName(String regionName){
-        this.regionName = regionName;
+    public void setName(String name){
+        this.name = name;
     }
 }
